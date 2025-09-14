56457d9701bb360dd37ed73e3402b9a38b982d2e
sowmyagampala <sowmyagampala77@gmail.com>
2025-09-14 11:01:49 +0530

    PrintF usage

---
+++ src/CodingChallenges/NinethTable.java
@@ -0,0 +1,12 @@
+package CodingChallenges;
+
+public class NinethTable {
+    public static void main(String[] args) {
+        int a=9;
+        System.out.printf("%d * %d = %d \n",a, 1,a);
+        System.out.printf("%d * %d = %d \n",a, 2,a);
+        System.out.printf("%d * %d = %d \n",a, 3,a);
+        System.out.printf("%d * %d = %d \n",a, 4,a);
+        System.out.printf("%d * %d = %d \n ",a, 5,a);
+    }
+}
